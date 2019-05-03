e9cda801c2ea3a25af55ee3c0ec73087be683f88
SergeyKamenshchikov <kamphys@gmail.com>
2019-05-03 14:11:59 +0300

    Add files via upload

---
+++ factoring.py
@@ -0,0 +1,195 @@
+﻿#import libraries# 
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import library as mylib
+from tabulate import tabulate
+#/import libraries
+
+#read xls into df#
+path='DATA/report.xlsx';
+df=pd.read_excel(path)
+#/read xls into df#
+
+#parameters#
+pca=1
+eda=1
+voting=3
+#/parameters#
+'''
+Features: ['Data' 'Visits' 'Viewers'  'Views' 'BR' 'Depth' 'Time' 'Views/Viewer']
+'''
+
+#Print parameters#
+mylib.parameters_out()
+#/Print parameters#
+
+#Print features,date#
+print('----------------------------')
+column_names=list(df.columns.values)
+print('All fields:',column_names)
+print('----------------------------')
+print('Data Start:',list(df['Data'])[0])
+print('Data End:',list(df['Data'])[-1])
+print('Set power:', len(df))
+print('----------------------------')
+#/Print features and data#
+
+#get/rescale exploratory features/target#
+X_data=[]
+X_data.extend([mylib.norm(list(df['Visits'])),mylib.norm(list(df['Viewers']))])
+X_data.extend([mylib.norm(list(df['Views']))]),X_data.append(mylib.norm(list(df['Depth'])))
+X_data.append(mylib.norm(list(df['Time'])))
+X_data.append(mylib.norm(list(np.array(list(df['Views']))/np.array(list(df['Viewers'])))))
+column_names.append('Views/Viewer')
+X_data=np.transpose(X_data)
+del column_names[5];column_names=column_names[2:]
+
+data_dict=dict(zip(column_names, X_data))
+data_df=pd.DataFrame(X_data,columns=column_names)#features
+fields=column_names
+Target=mylib.norm(list(df['CR']));Target=np.transpose(Target)
+print('Features:',fields)
+#get/rescale exploratory features#
+
+#exploratory analysis#
+if pca==1:
+    mylib.pca(column_names,data_df)
+
+if eda==1:
+    exp_df=data_df
+    exp_df['CR']=np.transpose(mylib.norm(list(df['CR'])))
+    mylib.cormap(exp_df)
+    mylib.eda(exp_df)
+    print('----------------------------')
+    for item in range(0,len(fields)):
+      print('Conversion vice '+str(fields[item])+'='+str(mylib.cf(X_data[:,item],Target)[0])+'%'+',p-significance='+str(mylib.cf(X_data[:,item],Target)[1]))
+#/exploratory analysis#
+
+'''
+Features 1: ['Visits', 'Viewers', 'Views']
+Features 2: ['Depth', 'Time', 'Views/Viewer']
+'''
+
+#split features#
+print('----------------------------')
+traffic=fields[0:3];print('Traffic features: '+str(traffic))
+retention=fields[3:6];print('Retention features: '+str(retention))
+df_traffic=pd.concat([data_df['Visits'], data_df['Viewers'], data_df['Views']], axis=1)#traffic
+df_retention=pd.concat([data_df['Depth'], data_df['Time'], data_df['Views/Viewer']], axis=1)#retention
+#/split features#
+
+#absolute scores#
+print('----------------------------')
+print('<Logreg>:')
+print('mean_score_traffic:'+str(mylib.logreg(df_traffic,Target)[0]))
+print('mean_score_retention:'+str(mylib.logreg(df_retention,Target)[0]))
+
+print('----------------------------')
+print('<Knn>:')
+print('mean_score_traffic:'+str(mylib.knn(df_traffic,Target)[0]))
+print('mean_score_retention:'+str(mylib.knn(df_retention,Target)[0]))
+
+print('----------------------------')
+print('<Naive Bayes>:')
+print('mean_score_traffic:'+str(mylib.naivebayes(df_traffic,Target)[0]))
+print('mean_score_retention:'+str(mylib.naivebayes(df_retention,Target)[0]))
+
+print('----------------------------')
+print('<Decision tree>:')
+print('mean_score_traffic:'+str(mylib.dtree(df_traffic,Target)[0]))
+print('mean_score_retention:'+str(mylib.dtree(df_retention,Target)[0]))
+
+print('----------------------------')
+print('<MLP>:')
+print('mean_score_traffic:'+str(mylib.mlp(df_traffic,Target)[0]))
+print('mean_score_retention:'+str(mylib.mlp(df_retention,Target)[0]))
+#/absolute scores#
+
+#delta scores#
+print('----------------------------')
+s=[]
+s.append(mylib.logreg(df_retention,Target)[0]-mylib.logreg(df_traffic,Target)[0])#logreg
+s.append(mylib.knn(df_retention,Target)[0]-mylib.knn(df_traffic,Target)[0])#knn
+s.append(mylib.dtree(df_retention,Target)[0]-mylib.dtree(df_traffic,Target)[0])#forest
+s.append(mylib.naivebayes(df_retention,Target)[0]-mylib.naivebayes(df_traffic,Target)[0])#nbayes
+s.append(mylib.mlp(df_retention,Target)[0]-mylib.mlp(df_traffic,Target)[0])#mlp
+
+s=list(np.round(100*np.array(s))/100)
+
+print('<Retention-Traffic, delta>:')
+print('logreg_delta:'+str(s[0]))
+print('knn_delta:'+str(s[1]))
+print('dtree_delta:'+str(s[2]))
+print('nb_delta:'+str(s[3]))
+print('mlp_delta:'+str(s[4]))
+print('\naverage_delta:'+str(np.round(100*np.average(np.array(s)))/100))
+print('std_delta:'+str(np.round(100*np.std(np.array(s)))/100))
+#/delta scores#
+
+#ensemble scores for retention#
+print('----------------------------')
+a=mylib.logreg(df_retention,Target)[1]
+b=mylib.knn(df_retention,Target)[1]
+c=mylib.dtree(df_retention,Target)[1]
+d=mylib.naivebayes(df_retention,Target)[1]
+e=mylib.mlp(df_retention,Target)[1]
+
+test_list=mylib.logreg(df_retention,Target)[2]
+ensemble_score=[]
+for item in zip(a,b,c,d,e):
+    if sum(item)>=voting: ensemble_score.append(1)
+    if sum(item)<voting: ensemble_score.append(0)
+
+m=0;n=0
+for i in range(len(test_list)):
+    m=m+1;
+    if ensemble_score[i]==test_list[i]:n=n+1
+
+print('Retention ensemble accuracy:'+str(round(100*n/m))+'%')
+#/ensemble scores for retention#
+
+#ensemble scores for traffic#
+print('----------------------------')
+a=mylib.logreg(df_traffic,Target)[1]
+b=mylib.knn(df_traffic,Target)[1]
+c=mylib.dtree(df_traffic,Target)[1]
+d=mylib.naivebayes(df_traffic,Target)[1]
+e=mylib.mlp(df_traffic,Target)[1]
+
+test_list=mylib.logreg(df_traffic,Target)[2]
+ensemble_score=[]
+for item in zip(a,b,c,d,e):
+    if sum(item)>=voting: ensemble_score.append(1)
+    if sum(item)<voting: ensemble_score.append(0)
+
+m=0;n=0
+for i in range(len(test_list)):
+    m=m+1;
+    if ensemble_score[i]==test_list[i]:n=n+1
+
+print('Traffic ensemble accuracy:'+str(round(100*n/m))+'%')
+#/ensemble scores for traffic#
+
+#FINISH#
+print('----------------------------')
+print('Finished')
+#/FINISH#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ library.py
@@ -0,0 +1,199 @@
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import grangercausalitytests as gct
+import scipy.stats
+import re
+from scipy.stats.stats import pearsonr
+from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from statistics import median
+from sklearn.model_selection import GridSearchCV
+import warnings
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+warnings.filterwarnings("ignore")
+
+#parameters#
+print('----------------------------')
+cv_n=10
+out_sample=0.5
+iterations=1000
+neigbors_maximum=100
+#parameters#
+
+def parameters_out():
+  print('cv_n='+str(cv_n))
+  print('out_sample='+str(out_sample))
+  print('iterations='+str(iterations))
+  print('neigbors_maximum='+str(neigbors_maximum))
+
+def adf(z):
+  result=adfuller(z)
+  delta=round(100*list(result[4].values())[1])/100-round(result[0])
+  print('dADF Statistic for 5%:'+str(round(100*delta)/100))
+  print('p-value: '+str(round(100*result[1]))+'%')
+
+def eda(x):
+  pd.plotting.scatter_matrix(x,figsize=[8,8],diagonal='kde',grid=True,ax=None,range_padding=1)
+  plt.show()
+
+def norm(x):
+  x=np.array(x)-min(x)
+  x=x/(max(x)-min(x))
+  x=[float("{0:.2f}".format(item)) for item in list(x)]
+  return x
+
+def timeconv(x):
+  sec=[];
+  for item in x:
+    h,m,s=re.split(':',item)
+    total=int(h)*3600+int(m)*60+int(s)
+    sec.append(total)
+  return sec
+
+def stand(x):
+  x=np.array(x)
+  x=x/np.std(x)
+  return x  
+
+def pca(x,y):
+  print('----------------------------')
+  pca=PCA()
+  pca.fit(y)
+  PCA(copy=True)
+  features=x
+  pca.explained_variance_=norm(pca.explained_variance_)
+  pca.explained_variance_=list(np.array(pca.explained_variance_)*100)
+  plt.bar(x, pca.explained_variance_,width=0.5)
+  plt.xticks(features);plt.ylabel('variance');plt.xlabel('PCA feature');
+
+  print('<PCA components>:')
+  for i in range(0,len(pca.explained_variance_)):
+    print(str(x[i])+': '+str(int(pca.explained_variance_[i]))+'%')
+    i=i+1
+  plt.show()
+
+def gct(y,x):
+  lag=1
+  matrix=np.column_stack([y,x])
+  gt=gct(matrix,3,verbose=False)
+  gr_test=[item for item in gt.get(lag)[0]['params_ftest']];
+  F_crit=int(round(scipy.stats.f.ppf(q=(1-gr_test[1]), dfn=gr_test[3], dfd=gr_test[2])))
+  print('p='+str(format(gr_test[1], '.2f')))#p-value
+  print("[F/F_crit]="+str(format(gr_test[0]/F_crit, '.2f')))#F/F_crit
+
+def cf(x,y):
+  CFV=pearsonr(x,y)
+  correlation=str(abs(round(int(100*CFV[0]))))#correlation
+  p_mystake=str(round((100*CFV[1]),2))#p-value
+  cf_mst=[correlation,p_mystake]
+  return cf_mst
+
+def cormap(x):
+  corr=x.corr()
+  fig = plt.figure(figsize=(10, 7))
+  g=sns.heatmap(corr,annot=True,linewidths=.2, cbar_kws={"orientation": "horizontal"})
+  #g.set_xticklabels('labels', rotation=30)
+  plt.show()
+
+def get_class(x):
+  threshold=median(list(x))
+  class_type=[]
+  for item in list(x):
+    if item>=threshold: label=1;class_type.append(label)
+    if item<threshold: label=0;class_type.append(label)
+  return class_type  
+
+def outlist(x):
+  scores=stand(x)
+  cv_score_mean=round(100*np.mean(x))/100;
+  return cv_score_mean
+
+def prediction(X_features,T,model):
+  X_train,X_test,y_train,y_test=train_test_split(X_features,T,test_size=out_sample,random_state=21,stratify=T)
+  model.fit(X_train,y_train)
+  y_pred=list(model.predict(X_test))
+  y_test=list(y_test)
+  return [y_pred,y_test]
+
+#############MODELS#############
+
+##LOGREG##
+def logreg(X_features,z):
+  T1=get_class(z)
+
+  scores=[]
+  for item in range(2,cv_n):
+    param_grid = {'tol': [0.1,0.01,0.001,0.0001]}
+    logreg=LogisticRegression()
+    logreg_cv=GridSearchCV(logreg, param_grid, scoring='roc_auc', cv=item)
+    logreg_cv.fit(X_features,T1)
+    tol_opt=logreg_cv.best_params_['tol']
+    logreg_final=LogisticRegression(tol=tol_opt)
+    cv_scores=list(cross_val_score(logreg_final,X_features,T1,cv=item,scoring='roc_auc'))
+
+  return ([outlist(cv_scores)]+prediction(X_features,T1,logreg))
+
+##FOREST##
+def dtree(X_features,z):
+  T2=get_class(z)
+
+  scores=[]
+  for item in range(2,cv_n):
+    dtree=DecisionTreeClassifier(criterion='entropy')
+    cv_scores=list(cross_val_score(dtree,X_features,T2,cv=item,scoring='roc_auc'))
+
+  return ([outlist(cv_scores)]+prediction(X_features,T2,dtree))
+
+##MLP##
+def mlp(X_features,z):
+  T3=get_class(z) 
+
+  scores=[]
+  for item in range(2,cv_n):
+    clf_final=MLPClassifier(activation='relu', hidden_layer_sizes=(3,1), max_iter=iterations, random_state=1, solver='lbfgs')
+    cv_scores=list(cross_val_score(clf_final,X_features,T3,cv=item,scoring='roc_auc'))
+
+  return ([outlist(cv_scores)]+prediction(X_features,T3,clf_final))
+
+##KNN##
+def knn(X_features,z):
+  T4=get_class(z)
+
+  scores=[]
+  for item in range(2,cv_n):
+    param_grid = {'n_neighbors': np.arange(2,neigbors_maximum)}
+    knn=KNeighborsClassifier()
+    knn_cv=GridSearchCV(knn, param_grid, scoring='roc_auc', cv=item)
+    knn_cv.fit(X_features,T4)
+    n_opt=knn_cv.best_params_['n_neighbors']
+    knn_final=KNeighborsClassifier(n_neighbors=n_opt)
+
+    cv_scores=list(cross_val_score(knn_final,X_features,T4,cv=item,scoring='roc_auc'))
+    scores.extend(cv_scores)
+
+  return ([outlist(cv_scores)]+prediction(X_features,T4,knn_final))
+
+##GNB##
+def naivebayes(X_features,z):
+  T5=get_class(z)
+
+  scores=[]
+  for item in range(2,cv_n):
+    gnb=GaussianNB()
+    cv_scores=list(cross_val_score(gnb,X_features,T5,cv=item,scoring='roc_auc'))
+    scores.extend(cv_scores)
+
+  return ([outlist(cv_scores)]+prediction(X_features,T5,gnb))
+#############/MODELS#############
+
